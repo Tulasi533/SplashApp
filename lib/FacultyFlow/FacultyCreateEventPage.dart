4b0f51f630b8f7d09fc0f64527af47a54e9a1ce2
Tulasi533 <annapurnatulasi95@gmail.com>
2022-04-09 19:42:11 +0530

    started create event page

---
+++ lib/FacultyFlow/FacultyCreateEventPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FacultyEventCreatePage extends StatefulWidget {
+  const FacultyEventCreatePage({ Key? key }) : super(key: key);
+
+  @override
+  State<FacultyEventCreatePage> createState() => _FacultyEventCreatePageState();
+}
+
+class _FacultyEventCreatePageState extends State<FacultyEventCreatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF09488D),
+    );
+  }
+}
