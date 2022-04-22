0a75c4e8716dee8ff698239b7c9b7768fa04320b
Tulasi533 <annapurnatulasi95@gmail.com>
2022-04-22 14:34:19 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: page,
+      home: page
     );
   }
 }
